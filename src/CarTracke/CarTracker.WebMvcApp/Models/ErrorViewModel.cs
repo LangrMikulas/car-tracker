caf9f0279d91425d4c9d0d1b3ba788c3f7e9ed59
Mikuláš Langr <LangrMikulas@litv.sssvt.cz>
2026-01-05 09:04:47 +0100

    feat: created project by template

---
+++ src/CarTracke/CarTracker.WebMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarTracker.WebMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
