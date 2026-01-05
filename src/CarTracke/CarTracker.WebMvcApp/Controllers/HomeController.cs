1ee7f6fe823e34e1abacd8eebaf8cdad981358c3
LangrMikulas <210342058+LangrMikulas@users.noreply.github.com>
2026-01-05 09:11:05 +0100

    chore: Add test comment in HomeController constructor
    Added a test comment in the HomeController constructor.

--- src/CarTracke/CarTracker.WebMvcApp/Controllers/HomeController.cs
+++ src/CarTracke/CarTracker.WebMvcApp/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace CarTracker.WebMvcApp.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
+            //testuju commit
             _logger = logger;
         }
 
