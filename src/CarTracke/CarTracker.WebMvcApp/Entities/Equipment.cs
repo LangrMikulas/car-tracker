0eeb50d71cfb0fd74081b6b349662a50851cec4b
santa420jedenahory <mukuasek@gmail.com>
2026-01-18 15:03:16 +0100

    addiotional commit

---
+++ src/CarTracke/CarTracker.WebMvcApp/Entities/Equipment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarTracker.WebMvcApp.Entities
+{
+    [Table ("Equipment")]
+    public class Equipment
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("brand")]
+        public string Brand { get; set; }
+
+        [Column("is_table")]
+        public bool IsTable { get; set; }
+    }
+}
---
+++ src/CarTracke/CarTracker.WebMvcApp/Entities/FuelType.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarTracker.WebMvcApp.Entities
+{
+    [Table ("fuelTypes")]
+    public class FuelType
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+    }
+}
