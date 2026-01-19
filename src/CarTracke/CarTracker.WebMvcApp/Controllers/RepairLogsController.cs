b3b0ae6dd83bab170fe4885b8e7eaa5077732005
langrmikulas <langrmikulas@L107PC09>
2026-01-19 09:45:13 +0100

    joj

---
+++ src/CarTracke/CarTracker.WebMvcApp/Controllers/RepairLogsController.cs
@@ -0,0 +1,38 @@
+﻿using AspNetCoreGeneratedDocument;
+using CarTracker.WebMvcApp.Contexts;
+using CarTracker.WebMvcApp.Entities;
+using CarTracker.WebMvcApp.Entities.Logs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarTracker.WebMvcApp.Controllers
+{
+    public class RepairLogsController : Controller
+    {
+        public AppDbContext AppDbContext { get; set; }
+        public List<RepairLog> RepairLogs { get; set; }
+
+        public RepairLogsController()
+        {
+            AppDbContext = new AppDbContext();
+            RepairLogs = AppDbContext.RepairLog.ToList();
+        }
+
+        public IActionResult ShowRepairLog(int? carId)
+        {
+            var logsQuery = AppDbContext.RepairLogs.Include(r => r.Car).AsQueryable();
+            if (carId.HasValue)
+            {
+                logsQuery = logsQuery.Where(r => r.CarId == carId);
+            }
+
+            var repairLogs = AppDbContext.RepairLog
+                .Include(r => r.Car)
+                .Include(r => r.User)
+                .ToList();
+            return View(RepairLogs);
+        }
+
+
+    }
+}
